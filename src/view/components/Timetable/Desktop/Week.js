a4f4938be36f922397c7ef9fab9540ee650c1e79
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-07-03 20:46:22 +0300

    ref Timetable: separated Class and TimeLine

--- src/view/components/Timetable/Desktop/Week.js
+++ src/view/components/Timetable/Desktop/Week.js
@@ -7,20 +7,11 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import AccessTimeOutlinedIcon from '@material-ui/icons/AccessTimeOutlined';
-import {withStyles} from "@material-ui/core/styles";
 
-
-const CLASS_NAME_MAX_LENGTH = 40;
-
-const styles = theme => ({
-    timeLine: {
-        border: "1px solid black",
-        minHeight: theme.spacing(5),
-    },
-});
+import Class from "./Class";
+import TimeLine from "./TimeLine";
 
 
 class Week extends React.Component {
@@ -31,52 +22,9 @@ class Week extends React.Component {
         this.state = {
             sortedClasses: universityClasses,
         }
-
-        this.renderClass = this.renderClass.bind(this);
-    }
-
-    renderClass(universityClass) {
-        if (universityClass)
-            return (
-                <Grid
-                    container
-                    direction="column"
-                    justify="space-between"
-                    alignItems="flex-start"
-                >
-                    <Grid item xs>
-                        <Typography variant="body2">
-                            <strong>
-                                {
-                                    universityClass.name.length > CLASS_NAME_MAX_LENGTH ?
-                                        universityClass.name.substring(0, CLASS_NAME_MAX_LENGTH - 2) + '..' :
-                                        universityClass.name
-                                }
-                            </strong>
-                        </Typography>
-                    </Grid>
-
-                    <Grid item xs>
-                        <Typography variant="caption">
-                            {universityClass.teacher.officialName}
-                        </Typography>
-                    </Grid>
-
-                    <Grid item xs>
-                        <Typography variant="caption">
-                            {universityClass.room.name}
-                            -{universityClass.room.universityBuilding} {universityClass.type}
-                        </Typography>
-                    </Grid>
-                </Grid>
-            )
-        else
-            return (<h6/>)
     }
 
     render() {
-        const { classes } = this.props;
-
         const tableTitle = (
             <TableCell align="center" colSpan={7}>
                 <Typography variant="h6">
@@ -102,31 +50,17 @@ class Week extends React.Component {
             [1, 2, 3, 4, 5].map(timeSlotId => (
                 <TableRow key={timeSlotId}>
                     <TableCell>
-                        <Grid
-                            container
-                            direction="column"
-                            justify="flex-start"
-                            alignItems="center"
-                        >
-                            <Grid item>
-                                {this.props.timeSlots.find(v => v.id === timeSlotId).timeStart}
-                            </Grid>
-
-                            <Grid item>
-                                <div className={classes.timeLine} />
-                            </Grid>
-
-                            <Grid item>
-                                {this.props.timeSlots.find(v => v.id === timeSlotId).timeEnd}
-                            </Grid>
-
-                        </Grid>
+                        <TimeLine
+                            timeSlot={this.props.timeSlots.find(v => v.id === timeSlotId)}
+                        />
                     </TableCell>
 
                     {
                         [0, 1, 2, 3, 4, 5].map(dayOfWeek => (
                             <TableCell component="th" scope="row" key={dayOfWeek}>
-                                {this.renderClass(this.state.sortedClasses[dayOfWeek][timeSlotId-1])}
+                                <Class
+                                    {...this.state.sortedClasses[dayOfWeek][timeSlotId-1]}
+                                />
                             </TableCell>
                         ))
                     }
@@ -162,37 +96,7 @@ class Week extends React.Component {
 }
 
 Week.propTypes = {
-    universityClasses: PropTypes.arrayOf(
-        PropTypes.arrayOf(
-            PropTypes.shape({
-                id: PropTypes.number.isRequired,
-
-                name: PropTypes.string.isRequired,
-                type: PropTypes.string.isRequired,
-
-                dayOfWeek: PropTypes.number.isRequired,
-                weekNumber: PropTypes.number.isRequired,
-
-                teacher: PropTypes.shape({
-                    id: PropTypes.number.isRequired,
-                    name: PropTypes.string.isRequired,
-                    officialName: PropTypes.string.isRequired,
-                }).isRequired,
-
-                room: PropTypes.shape({
-                    id: PropTypes.number.isRequired,
-                    name: PropTypes.string.isRequired,
-                    universityBuilding: PropTypes.number.isRequired,
-                }).isRequired,
-
-                timeSlot: PropTypes.shape({
-                    id: PropTypes.number.isRequired,
-                    timeStart: PropTypes.string.isRequired,
-                    timeEnd: PropTypes.string.isRequired,
-                }).isRequired,
-            })
-        )
-    ).isRequired,
+    universityClasses: PropTypes.array.isRequired,
 
     timeSlots: PropTypes.arrayOf(
         PropTypes.shape({
@@ -205,4 +109,4 @@ Week.propTypes = {
     title: PropTypes.string.isRequired,
 }
 
-export default withStyles(styles, { withTheme: true })(Week);
+export default Week;

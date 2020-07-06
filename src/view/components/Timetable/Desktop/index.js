761f8660be6e0ced12e63396ece1d31f944bdc5f
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-07-06 16:20:28 +0300

    ref timetable: add redux

--- src/view/components/Timetable/Desktop/index.js
+++ src/view/components/Timetable/Desktop/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Container from "@material-ui/core/Container";
 import Week from "./Week";
 import {withStyles} from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
@@ -21,7 +20,7 @@ class DesktopTimeTable extends React.Component {
             return [1, 2, 3, 4, 5, 6].map(currentDayOfWeek => {
                 let classesByDayOfWeek = classesByWeek.filter(v => v.dayOfWeek === currentDayOfWeek);
                 return [1, 2, 3, 4, 5].map(currentTimeSlotId =>
-                    classesByDayOfWeek.find(v => v.timeSlot.id === currentTimeSlotId)
+                    classesByDayOfWeek.find(v => v.timeSlot === currentTimeSlotId)
                 );
             })
         });
@@ -35,66 +34,34 @@ class DesktopTimeTable extends React.Component {
         const { classes } = this.props;
         
         return (
-            <Container maxWidth="lg" disableGutters>
+            <Box>
                 <Box className={classes.week}>
                     <Week
                         title="1 неделя"
-                        universityClasses={this.state.sortedClasses[0]}
+                        classes={this.state.sortedClasses[0]}
                         timeSlots={this.props.timeSlots}
+                        filter={this.props.filter}
                     />
                 </Box>
 
                 <Box className={classes.week}>
                     <Week
                         title="2 неделя"
-                        universityClasses={this.state.sortedClasses[1]}
+                        classes={this.state.sortedClasses[1]}
                         timeSlots={this.props.timeSlots}
+                        filter={this.props.filter}
                     />
                 </Box>
-            </Container>
+            </Box>
         )
     }
 
 }
 
 DesktopTimeTable.propTypes = {
-    universityClasses: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-
-            name: PropTypes.string.isRequired,
-            type: PropTypes.string.isRequired,
-
-            dayOfWeek: PropTypes.number.isRequired,
-            weekNumber: PropTypes.number.isRequired,
-
-            teacher: PropTypes.shape({
-                id: PropTypes.number.isRequired,
-                name: PropTypes.string.isRequired,
-                officialName: PropTypes.string.isRequired,
-            }).isRequired,
-
-            room: PropTypes.shape({
-                id: PropTypes.number.isRequired,
-                name: PropTypes.string.isRequired,
-                universityBuilding: PropTypes.number.isRequired,
-            }).isRequired,
-
-            timeSlot: PropTypes.shape({
-                id: PropTypes.number.isRequired,
-                timeStart: PropTypes.string.isRequired,
-                timeEnd: PropTypes.string.isRequired,
-            }).isRequired,
-        })
-    ).isRequired,
-
-    timeSlots: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            timeStart: PropTypes.string.isRequired,
-            timeEnd: PropTypes.string.isRequired
-        })
-    ).isRequired,
+    universityClasses: PropTypes.array.isRequired,
+    timeSlots: PropTypes.array.isRequired,
+    filter: PropTypes.string.isRequired,
 }
 
 export default withStyles(styles, { withTheme: true })(DesktopTimeTable);
--- src/view/components/Timetable/index.js
+++ src/view/components/Timetable/index.js
@@ -1,56 +1,85 @@
 import React from "react";
-import testData from "./testData";
+import { connect } from 'react-redux';
+import { withRouter } from "react-router-dom";
+import Container from "@material-ui/core/Container";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import actions from "../../../store/actions"
 import DesktopTimeTable from "./Desktop/index";
+import {filters} from "../../../store/actions/classes";
 
+const mapStateToProps = state => {
+    return {
+        hasError: state.timeSlots.hasError || state.classes.hasError,
+        isFetching: state.timeSlots.isFetching || state.classes.isFetching,
+
+        timeSlots: {
+            isFetching: state.timeSlots.isFetching,
+            hasError: state.timeSlots.hasError,
+            data: state.timeSlots.data,
+        },
+
+        classes: {
+            isFetching: state.classes.isFetching,
+            hasError: state.classes.hasError,
+            data: state.classes.data,
+        },
+    }
+};
 
 class TimeTable extends React.Component {
 
     constructor(props) {
-        super(props);
-
-        const timeSlots = [
-            {
-                id: 1,
-                timeStart: "08:30",
-                timeEnd: "10:05",
-            },
-            {
-                id: 2,
-                timeStart: "12:20",
-                timeEnd: "13:55",
-            },
-            {
-                id: 3,
-                timeStart: "12:20",
-                timeEnd: "13:55",
-            },
-            {
-                id: 4,
-                timeStart: "14:15",
-                timeEnd: "15:50",
-            },
-            {
-                id: 5,
-                timeStart: "16:10",
-                timeEnd: "17:45",
-            }
-        ];
-
+        super (props);
 
         this.state = {
-            timeSlots
-        };
+            currentFilter: null,
+            payload: null,
+        }
+    }
+
+    componentDidMount() {
+        const { group } = this.props.match.params;
+
+        if (group) {
+            this.props.dispatch(actions.classes.fetchClassesByGroupIfNeeded(group));
+            this.setState({
+                currentFilter: filters.BY_GROUP,
+                payload: group,
+            });
+        }
+
+        this.props.dispatch(actions.timeSlots.fetchTimeSlotsIfNeeded());
     }
 
     render() {
+        let currentState;
+
+        if (this.props.isFetching || !this.state.currentFilter)
+            currentState = (
+                <CircularProgress size="4rem" />
+            )
+        else if (this.props.hasError)
+            currentState = (
+                <h1>Error =(</h1>
+            )
+        else {
+            let classesData = this.props.classes.data[this.state.currentFilter][this.state.payload];
+            currentState = (
+                <DesktopTimeTable
+                    universityClasses={classesData}
+                    timeSlots={this.props.timeSlots.data}
+                    filter={this.props.currentFilter}
+                />
+            )
+        }
+
         return (
-            <DesktopTimeTable
-                universityClasses={testData}
-                timeSlots={this.state.timeSlots}
-            />
+            <Container maxWidth="lg" disableGutters>
+                {currentState}
+            </Container>
         )
     }
 
 }
 
-export default TimeTable;
+export default withRouter(connect(mapStateToProps)(TimeTable));

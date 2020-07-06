652c8ed0402cd2d4292e668db50b35639a832b22
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-07-06 14:33:34 +0300

    feat store

---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+import { applyMiddleware, compose, createStore} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger'
+import { createBrowserHistory } from 'history'
+import { routerMiddleware } from 'connected-react-router'
+
+import { createRootReducer } from './reducers';
+
+
+export const history = createBrowserHistory();
+
+const getMiddleware = () => {
+    if (process.env.NODE_ENV === 'production') {
+        return composeWithDevTools(applyMiddleware(routerMiddleware(history), thunk));
+    } else {
+        // Enable additional logging in non-production environments.
+        return composeWithDevTools(applyMiddleware(routerMiddleware(history), thunk, createLogger()))
+    }
+};
+
+export default function configureStore(preloadedState) {
+    const store = createStore(
+        createRootReducer(history),
+        preloadedState,
+        compose(
+            getMiddleware(),
+        ),
+    )
+
+    return store
+}

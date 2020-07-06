45afb15c3cde948e29121b0bf5334dae4591d8de
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-07-06 16:21:14 +0300

    ref index: add redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import "./index.css"
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
+import configureStore, { history } from './store'
+
 import App from "./view/App";
 
+const store = configureStore();
+
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App />
+        </ConnectedRouter>
+    </Provider>,
+
     document.getElementById('root')
 );

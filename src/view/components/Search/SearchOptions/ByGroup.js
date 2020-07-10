0a2fcb94a9f5a8394c1662d64349b348580a3cb9
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-07-10 20:36:42 +0300

    feat search by group: add search by api

--- src/view/components/Search/SearchOptions/ByGroup.js
+++ src/view/components/Search/SearchOptions/ByGroup.js
@@ -1,10 +1,56 @@
 import React from 'react';
+import {connect} from "react-redux";
+import { push } from 'connected-react-router'
+import {searchGroup, searchStop} from "../../../../store/actions/groups";
+import routes from "../../../../routes";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
+const mapStateToProps = state => {
+    return {
+        hasError: state.groups.hasError,
+        isFetching: state.groups.isFetching,
 
-export default class ByGroup extends React.Component {
+        lastQuery: state.groups.search.query,
+        searchData: state.groups.search.data,
+    }
+};
+
+class ByGroup extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            open: false,
+            redirect: null,
+        };
+
+        this.onSearch = this.onSearch.bind(this);
+        this.getSearchOptions = this.getSearchOptions.bind(this);
+    };
+
+    componentWillUnmount() {
+        this.props.dispatch(searchStop());
+    }
+
+    onSearch(e, value) {
+        console.log({value});
+        this.props.dispatch(push(routes.schedulePreviewByGroup.replace(':group', value.id)));
+    }
+
+    getSearchOptions(e) {
+        if (!this.state.open)
+            return;
+
+        let query = e.target.value;
+        if (query.length === 0 || query === this.props.lastQuery)
+            return;
+
+        this.props.dispatch(searchGroup(query));
+    }
+
     render() {
         return (
             <Grid
@@ -14,13 +60,52 @@ export default class ByGroup extends React.Component {
                 spacing={5}
             >
                 <Grid item>
-                    <TextField id="standard-basic" label="Имя Группы" placeholder="КМ-92" />
-                </Grid>
+                    <Autocomplete
+                        id="asynchronous-demo"
+                        open={this.state.open}
+                        openText={"опции"}
+                        loadingText={"поиск..."}
+                        noOptionsText={"не найдено"}
+                        onChange={this.onSearch}
+                        onOpen={(e) => {
+                            this.setState({
+                                open: true
+                            });
+                            this.getSearchOptions(e);
+                        }}
+                        onClose={() => {
+                            this.setState({
+                                open: false
+                            });
+                        }}
+                        getOptionSelected={(option, value) => option.id === value.id}
+                        getOptionLabel={(option) => option.name}
+                        options={this.props.searchData}
+                        loading={this.props.isFetching}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                style={{width: 150}}
+                                label="Имя Группы"
+                                placeholder="км-92"
+                                onChange={this.getSearchOptions}
+                                InputProps={{
+                                    ...params.InputProps,
+                                    endAdornment: (
+                                        <React.Fragment>
+                                            {this.props.isFetching ? <CircularProgress color="inherit" size={20} /> : null}
+                                            {params.InputProps.endAdornment}
+                                        </React.Fragment>
+                                    ),
+                                }}
+                            />
+                        )}
+                    />
 
-                <Grid item>
-                    <Button variant="contained" color="primary">Поиск</Button>
                 </Grid>
             </Grid>
         )
     }
 }
+
+export default connect(mapStateToProps)(ByGroup);

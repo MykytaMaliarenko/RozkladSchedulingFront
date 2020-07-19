2442f2069c1b2a46016ceed904325c705433b3e3
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-07-19 22:11:49 +0300

    feat api.bindings: add rooms binding

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 import Classes from "./bindings/classes";
 import TimeSlots from "./bindings/timeSlots";
 import Groups from "./bindings/groups";
+import Rooms from "./bindings/rooms";
 
 export default {
     Classes,
     TimeSlots,
-    Groups
+    Groups,
+    Rooms
 }

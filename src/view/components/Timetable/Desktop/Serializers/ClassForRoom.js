d08d0470511d19add4e3d9c49687f70b6660aacb
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-08-01 14:14:39 +0300

    feat ClassForRoom: add many groups per room

--- src/view/components/Timetable/Desktop/Serializers/ClassForRoom.js
+++ src/view/components/Timetable/Desktop/Serializers/ClassForRoom.js
@@ -2,17 +2,43 @@ import React from 'react';
 import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import Link from "@material-ui/core/Link";
+import Box from "@material-ui/core/Box";
+import Popover from "@material-ui/core/Popover";
+import ButtonBase from "@material-ui/core/ButtonBase";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import routes from "../../../../../routes";
 import {push} from "connected-react-router";
-import Link from "@material-ui/core/Link";
 
 const NAME_MAX_LENGTH = 30;
 
 class ClassForRoom extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            anchorEl: null,
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+        this.handlePopupClose = this.handlePopupClose.bind(this);
+    }
+
+    handleClick(event) {
+        this.setState({
+            anchorEl: event.currentTarget,
+        })
+    }
+
+    handlePopupClose() {
+        this.setState({
+            anchorEl: null
+        })
+    }
+
     render() {
-        const {name, teacher, group} = this.props;
+        const {name, teacher, groups} = this.props;
 
         let nameSection;
         if (name.length > NAME_MAX_LENGTH)
@@ -42,18 +68,63 @@ class ClassForRoom extends React.Component {
                 </Typography>
             )
 
-        let groupSection;
-        if (group) {
-            let groupUrl = routes.schedulePreviewByGroup.replace(':group', group.id)
-            groupSection = (
-                <Link
-                    variant="caption"
-                    component="button"
-                    onClick={() => this.props.dispatch(push(groupUrl))}
-                >
-                    {group.name}
-                </Link>
-            )
+        let groupsSection;
+        if (groups) {
+            let linksToAllGroups = groups.map((group, index) => {
+                let groupUrl = routes.schedulePreviewByGroup.replace(':group', group.id);
+                return (
+                    <Link
+                        key={group.id}
+                        variant="caption"
+                        component="button"
+                        onClick={() => this.props.dispatch(push(groupUrl))}
+                    >
+                        {index === groups.length - 1 ? group.name : group.name + ','}
+                    </Link>
+                )
+            });
+
+            if (groups.length > 3) {
+                const groupsShortName = (
+                    <Typography variant="caption">
+                        {groups.slice(0, 2).map(group => group.name).join() + ' ...'}
+                    </Typography>
+                )
+
+                groupsSection = (
+                    <Box>
+                        <ButtonBase
+                            style={{'padding': '2.5px'}}
+                            aria-describedby={'popover-' + this.props.id}
+                            onClick={this.handleClick}
+                            focusRipple
+                        >
+                            {groupsShortName}
+                        </ButtonBase>
+
+                        <Popover
+                            id={'popover-' + this.props.id}
+                            open={Boolean(this.state.anchorEl)}
+                            anchorEl={this.state.anchorEl}
+                            onClose={this.handlePopupClose}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'center',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'center',
+                            }}
+                        >
+                            <Box style={{'margin': '5px'}}>
+                                {linksToAllGroups}
+                            </Box>
+                        </Popover>
+                    </Box>
+                )
+            } else {
+                groupsSection = linksToAllGroups;
+            }
         }
 
         return (
@@ -72,7 +143,7 @@ class ClassForRoom extends React.Component {
                 </Grid>
 
                 <Grid item xs>
-                    {groupSection}
+                    {groupsSection}
                 </Grid>
             </Grid>
         )
@@ -80,6 +151,7 @@ class ClassForRoom extends React.Component {
 }
 
 ClassForRoom.propTypes = {
+    id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     type: PropTypes.string,
 
@@ -87,10 +159,12 @@ ClassForRoom.propTypes = {
         officialName: PropTypes.string.isRequired,
     }),
 
-    group: PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        name: PropTypes.string.isRequired,
-    }),
+    groups: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+        })
+    ),
 };
 
 export default connect()(ClassForRoom);

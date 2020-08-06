3dc4a138618a4e1e3a5cf343d8a86a60a13e9091
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-08-06 11:26:15 +0300

    ref routes: rename schedulePreviewByBuilding

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const homeRoutes = {
 
 const schedulePreviewRoutes = {
     schedulePreviewByGroup: "/schedule/group/:group",
-    schedulePreviewByUniversityBuilding: "/schedule/building/:buildingName",
+    schedulePreviewByBuilding: "/schedule/building/:building",
     schedulePreviewByTeacher: "/schedule/teacher/:teacher",
     schedulePreviewByRoom: "/schedule/room/:room",
 }

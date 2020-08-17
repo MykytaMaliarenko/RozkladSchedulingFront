732930f206a0c9eb541c1a8366ccebe72be49235
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-08-17 14:51:05 +0300

    ref ByRoom: change building input to select

--- src/view/components/Search/SearchOptions/ByRoom.js
+++ src/view/components/Search/SearchOptions/ByRoom.js
@@ -1,22 +1,35 @@
 import React from "react";
 import {connect} from "react-redux";
-import Grid from "@material-ui/core/Grid";
-import Autocomplete from "@material-ui/lab/Autocomplete";
-import TextField from "@material-ui/core/TextField";
-import CircularProgress from "@material-ui/core/CircularProgress";
 import {
     fetchAllBuildingsIfNeeded,
     fetchAllRoomsInBuildingIfNeeded
 } from "../../../../store/actions/rooms";
 import {push} from "connected-react-router";
 import routes from "../../../../routes";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import FormControl from "@material-ui/core/FormControl";
+import MenuItem from "@material-ui/core/MenuItem";
+import {withStyles} from "@material-ui/core/styles";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const mapStateToProps = state => {
     return {
-        roomsData: state.rooms.roomsByBuilding,
-        buildings: state.rooms.buildings,
+        rooms: state.rooms.roomsByBuilding.data,
+        isFetchingRooms: state.rooms.roomsByBuilding.isFetching,
+
+        buildings: state.rooms.buildings.data,
     }
 };
+
+const styles = () => ({
+    menuPaper: {
+        maxHeight: 250
+    }
+});
 
 class ByRoom extends React.Component {
     constructor(props) {
@@ -24,12 +37,11 @@ class ByRoom extends React.Component {
 
         this.state = {
             isSearchRoomOpen: false,
-            isSearchBuildingOpen: false,
 
-            selectedBuilding: null,
+            selectedBuilding: '',
         };
 
-        this.openRoom = this.openRoom.bind(this);
+        this.selectRoom = this.selectRoom.bind(this);
         this.selectBuilding = this.selectBuilding.bind(this);
     }
 
@@ -37,21 +49,33 @@ class ByRoom extends React.Component {
         this.props.dispatch(fetchAllBuildingsIfNeeded());
     }
 
-    openRoom(room) {
+    selectRoom(room) {
         this.props.dispatch(
             push(routes.schedulePreviewByRoom.replace(':room', room.id))
         );
     }
 
-    selectBuilding(building) {
+    selectBuilding(event) {
+        let value = event.target.value;
+        if (value === '')
+            return;
+
         this.setState({
-            selectedBuilding: building,
+            selectedBuilding: value,
         });
 
-        this.props.dispatch(fetchAllRoomsInBuildingIfNeeded(building));
+        this.props.dispatch(fetchAllRoomsInBuildingIfNeeded(value));
     }
 
     render() {
+        let {rooms, isFetchingRooms, buildings, classes} = this.props;
+
+        let buildingMenuItems = null;
+        if (Array.isArray(buildings))
+            buildingMenuItems = buildings.map(building => (
+                <MenuItem value={building}>{building}</MenuItem>
+            ))
+
         return (
             <Grid
                 container
@@ -61,14 +85,17 @@ class ByRoom extends React.Component {
             >
                 <Grid item>
                     <Autocomplete
+                        ListboxProps={{ style: { maxHeight: "15rem" }, position: "bottom-start" }}
                         disabled={!this.state.selectedBuilding}
                         open={this.state.isSearchRoomOpen}
-                        options={this.props.roomsData.data}
-                        loading={this.props.roomsData.isFetching}
+                        options={rooms.sort((a, b) => a.name.localeCompare(b.name))}
+                        loading={isFetchingRooms}
                         openText={"опции"}
                         loadingText={"поиск..."}
                         noOptionsText={"не найдено"}
-                        onChange={(e, value) => this.openRoom(value)}
+                        onChange={
+                            (e, value) => this.selectRoom(value)
+                        }
                         onOpen={() => {
                             this.setState({
                                 isSearchRoomOpen: true
@@ -102,48 +129,27 @@ class ByRoom extends React.Component {
                 </Grid>
 
                 <Grid item>
-                    <Autocomplete
-                        open={this.state.isSearchBuildingOpen}
-                        options={this.props.buildings.data}
-                        loading={this.props.buildings.isFetching}
-                        openText={"опции"}
-                        loadingText={"поиск..."}
-                        noOptionsText={"не найдено"}
-                        onChange={(e, value) => this.selectBuilding(value)}
-                        onOpen={() => {
-                            this.setState({
-                                isSearchBuildingOpen: true
-                            });
-                        }}
-                        onClose={() => {
-                            this.setState({
-                                isSearchBuildingOpen: false
-                            });
-                        }}
-                        getOptionSelected={(option, value) => option == value}
-                        getOptionLabel={(option) => option.toString()}
-                        renderInput={(params) => (
-                            <TextField
-                                {...params}
-                                style={{width: 100}}
-                                label="Здание"
-                                placeholder="16"
-                                InputProps={{
-                                    ...params.InputProps,
-                                    endAdornment: (
-                                        <React.Fragment>
-                                            {this.props.isFetching ? <CircularProgress color="inherit" size={20} /> : null}
-                                            {params.InputProps.endAdornment}
-                                        </React.Fragment>
-                                    ),
-                                }}
-                            />
-                        )}
-                    />
+                    <FormControl
+                        disabled={!Array.isArray(buildings)}
+                    >
+                        <InputLabel id="search-by-room-building-label">Здание</InputLabel>
+                        <Select
+                            value={this.state.selectedBuilding}
+                            style={{width: 100}}
+                            labelId="search-by-room-building-label"
+                            id="search-by-room-building-select"
+                            onChange={this.selectBuilding}
+                            MenuProps={{ classes: { paper: classes.menuPaper }  }}
+                        >
+                            {buildingMenuItems}
+                        </Select>
+                    </FormControl>
                 </Grid>
             </Grid>
         )
     }
 }
 
-export default connect(mapStateToProps)(ByRoom);
+export default connect(mapStateToProps)(
+    withStyles(styles, { withTheme: true })(ByRoom)
+);
